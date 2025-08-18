d4bd1bd6c7d0b6ff288baf1fdb144489564f7fa8
Medaminekthir <medaminekthirimsara727309207@gmail.com>
2025-08-18 02:12:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+document.querySelector('.gallery-images-present').addEventListener('click',function(){
+            window.location.href = "elpresente.html"; 
+})
+
+document.querySelector('.gallery-images-past').addEventListener('click',function(){
+            window.location.href = "https://google.com"; 
+})
+
+
+
+document.querySelector('.gallery-images-futur').addEventListener('click',function(){
+            window.location.href = "elfuturo.html"; 
+})
+
+
+
+document.querySelector('.gallery-images-perfecto').addEventListener('click',function(){
+            window.location.href = "https://google.com"; 
+})
